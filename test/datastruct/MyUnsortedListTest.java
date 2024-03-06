30c1d9125bbc96c06779d475a83336b28df1990f
baydouna <ali.baydoun@etu.univ-grenoble-alpes.fr>
2024-03-06 11:12:40 +0100

    changement

--- test/datastruct/MyUnsortedListTest.java
+++ test/datastruct/MyUnsortedListTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 public class MyUnsortedListTest {
 	UnsortedList<Integer> testList;
 	
+	//Jorane et Ali etaient là
 	@Before
 	public void geList() {
 		testList = MyUnsortedList.of(1, 2, 3, 4);
